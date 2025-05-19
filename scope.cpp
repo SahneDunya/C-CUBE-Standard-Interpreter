f9943a272ef1f724eec6623fabd4c8f6fc8cac0f
Sahne Dünya <ilgazgozen@gmail.com>
2025-05-19 19:55:50 +0300

    Delete scope.cpp

--- scope.cpp
+++
@@ -1,8 +0,0 @@
-#include "scope.hpp"
-
-namespace CCube {
-
-// Scope struct'ı şu anda karmaşık metotlar içermediği için bu dosya boş kalır.
-// Metotlar eklendiğinde buraya implementasyonları yazılacaktır.
-
-} namespace CCube
