8f4756982a82136630d21baedfaa911646fa042a
Sahne Dünya <ilgazgozen@gmail.com>
2025-05-20 10:13:11 +0300

    Update value.h

--- value.h
+++ value.h
@@ -8,9 +8,12 @@
 #include "c_cube_module.h" // C_CUBE_ModulePtr için
 
 // İleri bildirimler
- class C_CUBE_Object; // Nesne yönelimli kısım için
- class C_CUBE_Function; // Fonksiyonlar için
- class C_CUBE_Module;
+class GcObject;
+class C_CUBE_Object;
+class C_CUBE_Function;
+class C_CUBE_Class;
+class C_CUBE_Module; // Yeni eklenen modül sınıfı
+class Callable;
 
 // C-CUBE runtime değerlerini temsil eden sınıf/yapı
  std::variant kullanmak farklı tipleri tutmak için iyi bir yoldur
@@ -26,6 +29,8 @@ using Value = std::variant<
     std::shared_ptr<C_CUBE_Class>,
     std::shared_ptr<Callable>,
     std::shared_ptr<C_CUBE_Module>
+    std::shared_ptr<C_CUBE_List>,
+    std::shared_ptr<C_CUBE_Dict>,
     // ... diğer tipler (listeler, dictler vb.)
 >;
 using ValuePtr = std::shared_ptr<Value>; // Değerlere işaretçiler
@@ -45,12 +50,59 @@ using ValueType = std::variant<
     std::shared_ptr<C_CUBE_Object>,
     std::shared_ptr<C_CUBE_Function>,
     std::shared_ptr<C_CUBE_Class>,
-    std::shared_ptr<Callable>, // Fonksiyonlar ve built-in'ler için
+    std::shared_ptr<Callable>, 
+    std::shared_ptr<C_CUBE_Module>
+    std::shared_ptr<C_CUBE_List>,
+    std::shared_ptr<C_CUBE_Dict>,
     // ... diğer tipler (List, Dict, vb.)
     ModulePtr // Yeni eklenen modül tipi!
 >;
 
 // ValuePtr tanımınız da bu ValueType'ı kullanmalı.
 using ValuePtr = std::shared_ptr<ValueType>;
+
+class Value {
+public:
+    ValueType data;
+
+    // Constructor'lar
+    Value() : data(std::monostate{}) {} // Varsayılan olarak 'none'
+    Value(bool val) : data(val) {}
+    Value(double val) : data(val) {}
+    Value(const std::string& val) : data(val) {}
+    Value(std::string&& val) : data(std::move(val)) {}
+    // shared_ptr tabanlı tipler için constructor'lar
+    Value(std::shared_ptr<C_CUBE_Object> obj) : data(obj) {}
+    Value(std::shared_ptr<C_CUBE_Function> func) : data(func) {}
+    Value(std::shared_ptr<C_CUBE_Class> cls) : data(cls) {}
+    Value(std::shared_ptr<Callable> callable) : data(callable) {}
+    Value(ModulePtr module) : data(module) {} // Yeni ModulePtr constructor'ı
+
+    // GcObject'ten türemiş bir shared_ptr'ı güvenli bir şekilde GcPtr olarak döndürür.
+    // Çöp toplayıcı tarafından çağrılacaktır.
+    GcPtr getGcObject() const;
+
+    // Type checking helpers (isteğe bağlı, Utils.h'de de olabilir)
+    bool isNone() const { return std::holds_alternative<std::monostate>(data); }
+    bool isBool() const { return std::holds_alternative<bool>(data); }
+    bool isNumber() const { return std::holds_alternative<double>(data); }
+    bool isString() const { return std::holds_alternative<std::string>(data); }
+    bool isObject() const { return std::holds_alternative<std::shared_ptr<C_CUBE_Object>>(data); }
+    bool isFunction() const { return std::holds_alternative<std::shared_ptr<C_CUBE_Function>>(data); }
+    bool isClass() const { return std::holds_alternative<std::shared_ptr<C_CUBE_Class>>(data); }
+    bool isCallable() const { return std::holds_alternative<std::shared_ptr<Callable>>(data); }
+    bool isModule() const { return std::holds_alternative<ModulePtr>(data); }
+
+
+    // Type casting helpers (std::get kullanarak)
+    template<typename T>
+    const T& as() const { return std::get<T>(data); }
+
+    template<typename T>
+    T& as() { return std::get<T>(data); }
+};
+
+// Yorumlayıcı içinde Value nesnelerine işaret etmek için kullanılacak shared_ptr
+using ValuePtr = std::shared_ptr<Value>;
 
 #endif // C_CUBE_VALUE_H
